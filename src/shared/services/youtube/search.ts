1239ea1156cd39169d0ce6c60de9f06dcd717ec9
Helena Simson <68454497+ArkfallBravo@users.noreply.github.com>
2026-05-10 16:19:14 -0400

    fix: finds spotify and youtube
    added .env file and uses .env credentials for spotify and youtube

--- src/shared/services/youtube/search.ts
+++ src/shared/services/youtube/search.ts
@@ -1,39 +1,26 @@
 import { fetch } from "~/shared/utils/fetch";
-import { isDefined } from "~/shared/utils/types";
 
 import type { SearchFunction } from "../types";
-import type { SearchObject } from "./codecs";
+import { YOUTUBE_KEY } from "./auth";
+
+type SearchResponse = {
+	items?: { id: { videoId?: string } }[];
+};
 
 export const search: SearchFunction = async ({ artist, title }) => {
-	const response = await fetch({
-		url: "https://youtube.com/results",
-		urlParameters: {
-			search_query: `${artist} ${title}`,
-		},
-		credentials: "omit",
-	});
+	const response = JSON.parse(
+		await fetch({
+			url: "https://www.googleapis.com/youtube/v3/search",
+			urlParameters: {
+				part: "id",
+				q: `${artist} ${title}`,
+				type: "video",
+				maxResults: "1",
+				key: YOUTUBE_KEY,
+			},
+		}),
+	) as SearchResponse;
 
-	const html = new DOMParser().parseFromString(response, "text/html");
-	const scripts = [...html.getElementsByTagName("script")];
-	const dataScript = scripts.find((script) =>
-		script.textContent?.includes("var ytInitialData"),
-	)?.textContent;
-	if (dataScript === null || dataScript === undefined)
-		throw new Error("Could not find search metadata tag");
-
-	const searchObjectString = [
-		...(/var ytInitialData = ({.*});/.exec(dataScript) ?? []),
-	][1];
-	if (!searchObjectString) throw new Error("Could not find search metadata");
-
-	const searchObject = JSON.parse(searchObjectString) as SearchObject;
-	const videoData =
-		searchObject.contents.twoColumnSearchResultsRenderer.primaryContents
-			.sectionListRenderer.contents[0]?.itemSectionRenderer?.contents;
-	if (!videoData) throw new Error("Could not find video data");
-
-	const videoId = videoData
-		.map((video) => video.videoRenderer)
-		.find(isDefined)?.videoId;
+	const videoId = response.items?.[0]?.id?.videoId;
 	return videoId ? `https://www.youtube.com/watch?v=${videoId}` : undefined;
 };
